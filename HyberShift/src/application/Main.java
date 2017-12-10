7a16c1317c72b6af5baa07b449b15faaf8d32ba6
Kiet Vo <geforce2409@gmail.com>
2017-12-10 22:34:29 +0700

    Commit code cho GUI Login và Register

--- HyberShift/src/application/Main.java
+++ HyberShift/src/application/Main.java
@@ -1,14 +1,22 @@
 package application;
 	
+import java.awt.Event;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 
+import com.jfoenix.controls.JFXButton;
+
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.fxml.FXMLLoader;
 import login.RegisterSceneController;
 
@@ -16,13 +24,38 @@ import login.RegisterSceneController;
 public class Main extends Application {
 	//static Pane root;
 	static Stage stg;
+	
+	private double xOffset = 0;
+	private double yOffset = 0;
+	
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("LoginScene.fxml"));	
+			Parent root = FXMLLoader.load(getClass().getResource("Register.fxml"));	
 			this.stg = primaryStage;
-			primaryStage.setTitle("Log in");
-			primaryStage.setScene(new Scene(root, 900, 500));
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			
+			//Kéo thả, di chuyển frame
+		    root.setOnMousePressed(new EventHandler<javafx.scene.input.MouseEvent>() {
+		        @Override
+		        public void handle(javafx.scene.input.MouseEvent event) {
+		            xOffset = event.getSceneX();
+		            yOffset = event.getSceneY();
+		        }
+		    });
+
+		    root.setOnMouseDragged(new EventHandler<javafx.scene.input.MouseEvent>() {
+		        @Override
+		        public void handle(javafx.scene.input.MouseEvent event) {
+		            primaryStage.setX(event.getScreenX() - xOffset);
+		            primaryStage.setY(event.getScreenY() - yOffset);
+		        }
+		    });
+			
+			Scene scene  = new Scene(root);
+			scene.setFill(Color.TRANSPARENT);
+			primaryStage.setScene(scene);
 			primaryStage.show();
 			//Scene scene = new Scene(root,600,400);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
@@ -36,7 +69,7 @@ public class Main extends Application {
 	
 	public static void showRegisterScene() throws IOException{
 		try {
-		    FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/login/RegisterScene.fxml"));
+		    FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/login/Register.fxml"));
 		    Parent root = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
             stage.setScene(new Scene(root));  
--- HyberShift/src/application/RegisterController.java
+++ HyberShift/src/application/RegisterController.java
@@ -1,5 +1,311 @@
 package application;
 
-public class RegisterController {
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.github.nkzawa.emitter.Emitter.Listener;
+import com.github.nkzawa.socketio.client.Socket;
+import com.jfoenix.controls.JFXButton;
+
+import chatsocket.ChatSocket;
+import dataobject.UserInfo;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class RegisterController implements Initializable{
+	@FXML private JFXButton btnLogin, btnRegister, btnExit;	
+	@FXML private AnchorPane pn_Login, pn_Register;
+	
+	//Register
+	@FXML TextField tfEmail;
+	@FXML TextField tfName;
+	@FXML TextField tfPassword;
+	@FXML TextField tfConfirmPassword;
+	@FXML TextField tfPhoneNumber;
+	@FXML Button btnConfirm;
+	
+	Socket socket;
+	UserInfo userInfo = UserInfo.getInstance();
+	
+	//Login
+	Main main;
+	
+	@FXML private TextField tfEmailLogin;
+	@FXML private TextField tfPasswordLogin;
+	@FXML private Button btnSignin;
+	@FXML private Hyperlink hlRegister;
+
+	ChatSocket chatsocket;
+	Runnable socketRunnable;
+	
+	public RegisterController() {
+		initSocket();
+		Thread socketThread = new Thread(socketRunnable);
+		socketThread.start();
+		
+		socket.on("authentication_result", new Listener() {
+			
+			@Override
+			public void call(Object... args) {
+				JSONObject data = (JSONObject) args[0];
+				System.out.println(data);
+                String result;
+                try {
+                	if (data != null){
+                    userInfo.setFullName(data.getJSONObject("userRecord").getString("displayName"));
+                                    
+                    	Platform.runLater(new Runnable(){
+    						@Override
+    						public void run() {		
+    							Main.showMainFromLoginScene();
+    						}       	 
+                        });   	
+                	}
+                	else
+                		Platform.runLater(new Runnable(){
+    						@Override
+    						public void run() {		
+    							new Alert(AlertType.ERROR, "Your email or password not valid. Please try again").show();
+    						}       	 
+                        });  
+                } catch (JSONException e) {
+                	e.printStackTrace();
+                    return;
+                }	
+				
+			}
+		});
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@FXML public void handleButtonAction(javafx.event.ActionEvent event) {
+		if (event.getSource() == btnLogin) {
+			pn_Login.toFront();
+			Login();
+		} 
+		else if (event.getSource() == btnRegister ) {
+				pn_Register.toFront();
+				Register();
+		}
+		else if (event.getSource() == btnExit) {
+		    // get a handle to the stage
+		    Stage stage = (Stage) btnExit.getScene().getWindow();
+		    // do what you have to do
+		    stage.close();
+		}
+	}
+	
+
+	//////////////////////////////// Login Panel ////////////////////////////////
+	
+	private void Login() {
+		// TODO Auto-generated method stub
+		initSocket();
+		Thread socketThread = new Thread(socketRunnable);
+		socketThread.start();
+		
+		socket.on("authentication_result", new Listener() {
+			
+			@Override
+			public void call(Object... args) {
+				JSONObject data = (JSONObject) args[0];
+				System.out.println(data);
+                String result;
+                try {
+                	if (data != null){
+                    userInfo.setFullName(data.getJSONObject("userRecord").getString("displayName"));
+                                    
+                    	Platform.runLater(new Runnable(){
+    						@Override
+    						public void run() {		
+    							Main.showMainFromLoginScene();
+    						}       	 
+                        });   	
+                	}
+                	else
+                		Platform.runLater(new Runnable(){
+    						@Override
+    						public void run() {		
+    							new Alert(AlertType.ERROR, "Your email or password not valid. Please try again").show();
+    						}       	 
+                        });  
+                } catch (JSONException e) {
+                	e.printStackTrace();
+                    return;
+                }	
+				
+			}
+		});
+	}
+	
+	@FXML
+	public void onActionBtnSigin(){
+		System.out.println("BtnSigin clicked");
+		if (!isValidLogin()){
+			new Alert(AlertType.WARNING, "Something went wrong with your information. Please check again").show();
+			return;
+		}
+		
+		//Convert to JSONObject
+		JSONObject userjson = new JSONObject();
+		try {
+			userjson.put("email", tfEmailLogin.getText());
+			userjson.put("password", tfPasswordLogin.getText());
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		socket.emit("authentication", userjson);
+	}
+	
+	@FXML
+	public void onActionHyberlinkRegister(){
+		try {
+			Main.showRegisterScene();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void initSocket(){
+		chatsocket = ChatSocket.getInstance();
+		socket = ChatSocket.getSocket();
+		socketRunnable = new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				//set up event
+				socket.on(Socket.EVENT_CONNECT, new Listener() {
+					
+					@Override
+					public void call(Object... args) {
+						// TODO Auto-generated method stub
+						System.out.println("Client connected to server");
+					}
+				}).on(Socket.EVENT_DISCONNECT, new Listener() {
+					
+					@Override
+					public void call(Object... args) {
+						// TODO Auto-generated method stub
+						System.out.println("Client disconnected to server");
+					}
+				});
+				
+				socket.connect();
+			}
+		};
+	}
+	
+	private boolean isValidLogin(){
+		
+		if (tfEmailLogin.getText().trim().length() == 0)
+			return false;
+		
+		if (tfPasswordLogin.getText().trim().length() == 0)
+			return false;
+		
+		return true;
+	}
+	
+	
+	//////////////////////////////// Register Panel //////////////////////////////////
+	public void Register() { 
+		ChatSocket.getInstance();
+		socket = ChatSocket.getSocket();
+			
+		socket.on("register_result", new Listener() {
+			
+			@Override
+			public void call(Object... args) {
+				JSONObject data = (JSONObject) args[0];
+	            String message;
+	            try {
+	                message = data.getString("message");
+	                System.out.println(message);
+	                Platform.runLater(new Runnable(){
+						@Override
+						public void run() {		
+							new Alert(AlertType.INFORMATION, message).show();
+						}       	 
+	                });
+	               
+	            } catch (JSONException e) {
+	            	e.printStackTrace();
+	                return;
+	            }	
+			}
+		});
+	}
+	
+	public void onActionBtnConfirm(){
+		System.out.println("BtnConfirm clicked");
+		
+		if (!isValidRegister()){
+			new Alert(AlertType.WARNING, "Something went wrong with your information. Please check again").show();
+			return;
+		}
+		//Push data
+		userInfo = UserInfo.getInstance();
+		
+		userInfo.setEmail(tfEmail.getText().toString());
+		userInfo.setPassword(tfPassword.getText().toString());
+		userInfo.setPhone(tfPhoneNumber.getText().toString());
+		userInfo.setFullName(tfName.getText().toString());
+		//Convert to JSONObject
+		JSONObject userjson = new JSONObject();
+		try {
+			//userjson.put("userid", userInfo.getUserid());
+			userjson.put("email", userInfo.getEmail());
+			userjson.put("fullname", userInfo.getFullName());
+			userjson.put("password", userInfo.getPassword());
+			userjson.put("phone", userInfo.getPhone());
+			userjson.put("linkavatar", userInfo.getLinkAvatar());
+			
+			socket.emit("register", userjson);
+			Main.showMainFromRegister();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+	}
+	
+	private boolean isValidRegister(){
+		System.out.println(tfEmail.getText().trim().length());
+		if (tfEmail.getText().trim().length() == 0)
+			return false;
+		if (tfName.getText().trim().length() == 0)
+			return false;
+		if (tfPassword.getText().trim().length() == 0)
+			return false;
+		if (tfConfirmPassword.getText().trim().length() == 0)
+			return false;
+		if (tfPhoneNumber.getText().trim().length() == 0)
+			return false;
+		if (!tfPassword.getText().toString().equals(tfConfirmPassword.getText().toString()))
+			return false;
+		return true;
+	}
 }
